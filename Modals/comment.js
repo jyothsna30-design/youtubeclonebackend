66ffd9fbfd3c6d344fdcde22774e1e08d0884f14
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2026-01-18 16:14:51 +0300

    comment model

---
+++ Modals/comment.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    },
+    video : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "video",
+        required: true
+    },
+    message:{
+        type: String,
+        required: true
+    }
+},{timestamps:true});
+
+export const Comment = mongoose.model("comment",commentSchema);
