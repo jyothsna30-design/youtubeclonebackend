bb8b38c652e0f70580911a51d28fe7f7fb1cce61
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2026-01-18 16:25:24 +0300

    added some user routes

--- Routes/user.js
+++ Routes/user.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { signUp , signIn ,logOut } from '../Controllers/user.js';
 
 const router = express.Router();
-
+// User routes
 router.post('/signup', signUp);
 router.post('/login', signIn);
 router.post('/logout', logOut);
