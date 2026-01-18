348b298a8b015fd96fc552a0e1f4b38518dac0f8
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2026-01-18 16:24:55 +0300

    added some  comment routes

--- Routes/comment.js
+++ Routes/comment.js
@@ -1,6 +1,8 @@
 import express  from 'express';
 import {addComment, getCommentsByVideoId} from '../Controllers/comment.js';
 import authenticate from '../middleware/authentication.js';
+
+// Comment routes
 const router = express.Router();
 router.post('/comment',authenticate,addComment);
 router.get('/comment/:videoId',getCommentsByVideoId);
