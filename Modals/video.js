5b9aa152839ed497425e9472827e9e987d42230c
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2026-01-18 16:15:59 +0300

    video model

---
+++ Modals/video.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema({
+    user : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true},
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true},
+
+    description:{
+        type: String,
+        required: true},
+    
+    videoLink:{
+        type: String,
+        required: true},
+    thumbnail:{
+        type: String,
+        required: true},
+    category:{
+        type: String,
+        default: "All"},
+    like:{
+        type: Number,
+        default: 0},
+    dislike:{
+        type: Number,
+        default: 0},
+   
+
+},{timestamps:true});
+export const Video = mongoose.model("video",videoSchema);
